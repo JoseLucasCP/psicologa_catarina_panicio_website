7c5a45fe237aa0919b7f627c5f62ea71d6f13d06
José Lucas C Peraro <joselucasperaro15@gmail.com>
2026-09-23 15:39:58 -0300

    feat: ajustes na responsividade para mobile, menus e css

--- js/script.js
+++ js/script.js
@@ -1,1 +1,26 @@
-console.log("Teste do site");
+const menuToggle = document.querySelector(".menu-toggle");
+const navMenu = document.querySelector(".nav-menu");
+const navLinks = document.querySelectorAll(".nav-menu a");
+
+menuToggle.addEventListener("click", function() {
+    navMenu.classList.toggle("active");
+
+    const menuAberto = navMenu.classList.contains("active");
+
+    menuToggle.setAttribute(
+        "aria-expanded",
+        menuAberto
+    );
+});
+
+/*Fechar o menu ao clicar em algum link*/
+navLinks.forEach(function (link) {
+    link.addEventListener("click", function() {
+        navMenu.classList.remove("active");
+
+        menuToggle.setAttribute(
+            "aria-expanded",
+            "false"
+        );
+    });
+});
